d1fcf8d5b03055a76af017dc7e17eb6768608931
difegam <difegam@gmail.com>
2023-05-13 21:48:21 +0200

    add FormView

--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,9 @@
 from typing import Any
 
 from django.db.models.query import QuerySet
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
-from django.views import View
-from django.views.generic import DetailView, ListView
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, FormView, ListView
 from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
@@ -14,22 +12,29 @@ from .models import Review
 
 # DJango Class base views
 # https://docs.djangoproject.com/en/4.2/topics/class-based-views/
-class ReviewView(View):
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = reverse_lazy("reviews:thank-you-page")
 
-    def get(self, request):
-        form = ReviewForm()
-        return self.render_view(request, form)
+    def form_valid(self, form: Any) -> HttpResponse:
+        form.save()
+        return super().form_valid(form)
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse("reviews:thank-you-page"))
-        return self.render_view(request, form)
 
-    def render_view(self, request, form):
-        return render(request, template_name="reviews/review.html", context={"form": form})
+#     def get(self, request):
+#         form = ReviewForm()
+#         return self.render_view(request, form)
 
+#     def post(self, request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse("reviews:thank-you-page"))
+#         return self.render_view(request, form)
+
+#     def render_view(self, request, form):
+#         return render(request, template_name="reviews/review.html", context={"form": form})
 
 # class ThankYouView(View):
 #     def get(self, request, *args, **kwargs):
