3d6a2598797acac90bfdd2185b85a3101cf9c784
difegam <difegam@gmail.com>
2023-06-05 20:49:44 +0200

    Add FileFiled to UserProfile model

--- profiles/views.py
+++ profiles/views.py
@@ -1,19 +1,16 @@
-from pathlib import Path
-
-from django.core.files.uploadedfile import UploadedFile
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
 from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
-
-def store_file(file: UploadedFile):
-    with Path("temp/image.jpg").open("wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+# def store_file(file: UploadedFile):
+#     with Path("temp/image.jpg").open("wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 
 class CreateProfileView(View):
@@ -23,10 +20,12 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html", context={"form": form})
 
     def post(self, request):
-        submitted_form = ProfileForm()
+        submitted_form = ProfileForm(request.POST, request.FILES)
 
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
+            # store_file(request.FILES["image"])
+            profile = UserProfile(image=request.FILES["user_image"])
+            profile.save()
             return HttpResponseRedirect("/profiles")
 
         return render(request, "profiles/create_profile.html", context={"form": submitted_form})
