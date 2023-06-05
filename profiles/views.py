25c37f75047ae42dab056e24dc62f2b46b3d8d2a
difegam <difegam@gmail.com>
2023-06-05 21:23:57 +0200

    replace view for CreateView

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.views import View
+# from django.shortcuts import render
+# from django.views import View
+from django.views.generic.edit import CreateView
 
-from .forms import ProfileForm
 from .models import UserProfile
 
 # Create your views here.
@@ -12,20 +11,26 @@ from .models import UserProfile
 #         for chunk in file.chunks():
 #             dest.write(chunk)
 
+# class CreateProfileView(View):
 
-class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", context={"form": form})
 
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", context={"form": form})
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
 
-    def post(self, request):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+#         if submitted_form.is_valid():
+#             # store_file(request.FILES["image"])
+#             profile = UserProfile(image=request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
 
-        if submitted_form.is_valid():
-            # store_file(request.FILES["image"])
-            profile = UserProfile(image=request.FILES["user_image"])
-            profile.save()
-            return HttpResponseRedirect("/profiles")
+#         return render(request, "profiles/create_profile.html", context={"form": submitted_form})
 
-        return render(request, "profiles/create_profile.html", context={"form": submitted_form})
+
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
